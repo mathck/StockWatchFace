1ca6050435371c22036a2fda3391a08727461837
Mateusz Czernecki <mathck@gmail.com>
2015-03-21 16:01:53 +0100

    Initial commit based on android sdk watchface sample

---
+++ WatchFace/Application/build/generated/source/buildConfig/test/debug/com/mathck/android/wearable/stockmonitor/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mathck.android.wearable.stockmonitor.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mathck.android.wearable.watchface.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
